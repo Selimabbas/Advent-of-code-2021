3aeaeb2aedface6d54a438b98c9a0ee13ef7d198
Selim Abbas Said <selim093@hotmail.fr>
2021-12-05 15:13:44 +0100

    day 4 part 2

--- src/main/kotlin/day04/part2.kt
+++ src/main/kotlin/day04/part2.kt
@@ -14,16 +14,21 @@ fun main() {
                 .filter { it.isNotEmpty() }
                 .map { it.toInt() }
         }
+    val bingoBoards = mutableListOf<Int>()
     for (i in 5..numbers.size) {
         val usedNumbers = numbers.subList(0, i)
-        for (boardIndex in 0 until boards.size / 5) {
+        loop@ for (boardIndex in 0 until boards.size / 5) {
+            if (bingoBoards.contains(boardIndex)) continue@loop
             val board = boards.subList(boardIndex * 5, boardIndex * 5 + 5)
             for (k in 0..4) {
                 if (usedNumbers.containsAll(board[k])
-                    || usedNumbers.containsAll(board.map { it[k] })
+                    || usedNumbers.containsAll(board.map { it[k] }.toList())
                 ) {
-                    println(bingo(board, usedNumbers))
-                    return
+                    if (!bingoBoards.contains(boardIndex)) bingoBoards.add(boardIndex)
+                    if (bingoBoards.size == boards.size / 5) {
+                        println(bingo(board, usedNumbers))
+                        return
+                    }
                 }
             }
         }
