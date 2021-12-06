aa91d0c075b135e053c3dffecd58807329a4a934
selim.abbassaid <selim.abbassaid@adevinta.com>
2021-12-06 12:47:51 +0100

    day06 part1

---
+++ src/main/kotlin/day06/part1.kt
@@ -0,0 +1,24 @@
+package day06
+
+import java.io.File
+
+fun main() {
+    val fishes = File("src/main/kotlin/day06/input.txt")
+            .readText()
+            .split(",")
+            .map { it.toInt() }
+            .toMutableList()
+
+    var day = 0
+    while (day < 80) {
+        day++
+        for (i in 0 until fishes.size) {
+            val fish = fishes[i]
+            if (fish == 0) {
+                fishes[i] = 6
+                fishes.add(8)
+            } else fishes[i]--
+        }
+    }
+    println(fishes.size)
+}
